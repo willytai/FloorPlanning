0607d9e4301b918fff173c187f0af3d714173631
Willy <b04502139@ntu.edu.tw>
2019-05-17 17:37:58 +0800

    optimized golden

---
+++ src/run.sh
@@ -0,0 +1,5 @@
+../bin/fp 0.5 ../input_pa3/apte.block ../input_pa3/apte.nets ../apte.out
+../bin/fp 0.5 ../input_pa3/hp.block ../input_pa3/hp.nets ../hp.out
+../bin/fp 0.5 ../input_pa3/xerox.block ../input_pa3/xerox.nets ../xerox.out
+../bin/fp 0.5 ../input_pa3/ami33.block ../input_pa3/ami33.nets ../ami33.out
+../bin/fp 0.5 ../input_pa3/ami49.block ../input_pa3/ami49.nets ../ami49.out
